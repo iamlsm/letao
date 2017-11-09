7b2f021e7c820fbe4b72427aa317f3c050ae79b1
lsm <915744317@qq.com>
2017-11-09 15:14:25 +0800

    提交第一天的代码

--- public/back/js/comment.js
+++ public/back/js/comment.js
@@ -10,3 +10,46 @@ $(document).ajaxStop(function () {
     NProgress.done();
   }, 500);
 });
+
+// 检测是否是登陆页
+if(location.href.indexOf('login.html') == -1){
+  $.ajax({
+    url:'/employee/checkRootLogin',
+    type:'get',
+    success:function(data){
+      // console.log(data);
+      if(data.error==400){
+        location.href = "login.html";
+      }
+      
+    }
+  })
+
+}
+
+$('.btn_menu').on('click',function(){
+  $('.lt-slide').toggleClass('now');
+  $('.lt-main').toggleClass('now');
+})
+
+$('.child').prev().on('click',function(){
+  $(this).next().slideToggle();
+})
+
+$('.btn_layout').on("click",function(){
+  $(".modal").modal();
+  })
+  $('.btn_confirm').on('click',function(){
+    // console.log('hhh');
+    $.ajax({
+      url:'/employee/employeeLogout',
+      success:function(data){
+        if(data.success){
+          location.href="login.html";
+  
+        }
+      }
+    })
+})
+
+
